0410d2fc952d745276cb9f3672e01bcd2e95b359
inikulin <ifaaan@gmail.com>
2019-07-08 22:47:14 +0100

    Implement byte pattern parsing from int literal

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,24 @@
 #[macro_use]
 mod test_utils {
     macro_rules! curry_parse_macros {
-        ($AstNode:ident) => {
+        // HACK: because of https://github.com/rust-lang/rust/issues/35853 we
+        // need to pass `$` as an argument to macro.
+        ($d:tt $AstNode:ident) => {
             macro_rules! parse {
-                ($t:tt) => {
-                    syn::parse_str::<$AstNode>(stringify!($t))
+                ($d ($d t:tt)+) => {
+                    syn::parse_str::<$AstNode>(stringify!($d ($d t)*))
                 };
             }
 
             macro_rules! parse_ok {
-                ($t:tt) => {
-                    parse!($t).unwrap()
+                ($d ($d t:tt)*) => {
+                    parse!($d ($d t)*).unwrap()
                 };
             }
 
             macro_rules! parse_err {
-                ($t:tt) => {
-                    format!("{}", parse!($t).unwrap_err())
+                ($d ($d t:tt)*) => {
+                    format!("{}", parse!($d ($d t)*).unwrap_err())
                 };
             }
         };
--- src/patterns/byte.rs
+++ src/patterns/byte.rs
@@ -1,20 +1,33 @@
+use std::convert::TryFrom;
 use syn::parse::{Parse, ParseStream};
-use syn::{Error as ParseError, LitChar, Result as ParseResult};
+use syn::{Error as ParseError, LitChar, LitInt, Result as ParseResult};
 
 const ERR_CHAR_IS_NOT_ASCII: &str = "character pattern should be an ASCII character";
+
+const ERR_INT_IS_OUT_OF_BOUNDS: &str =
+    "numeric pattern is not in the byte value range (0x00-0xFF, 0-255, etc.)";
 
 #[derive(PartialEq, Debug)]
 pub struct BytePattern(u8);
 
 impl BytePattern {
     fn parse_from_char_literal(input: ParseStream) -> ParseResult<Self> {
-        let lit_ch = input.parse::<LitChar>()?;
-        let ch = lit_ch.value();
+        let lit = input.parse::<LitChar>()?;
+        let ch = lit.value();
 
         if ch.is_ascii() {
             Ok(BytePattern(ch as u8))
         } else {
-            Err(ParseError::new_spanned(lit_ch, ERR_CHAR_IS_NOT_ASCII))
+            Err(ParseError::new_spanned(lit, ERR_CHAR_IS_NOT_ASCII))
+        }
+    }
+
+    fn parse_from_int_literal(input: ParseStream) -> ParseResult<Self> {
+        let lit = input.parse::<LitInt>()?;
+
+        match u8::try_from(lit.value()) {
+            Ok(byte) => Ok(BytePattern(byte)),
+            Err(_) => Err(ParseError::new_spanned(lit, ERR_INT_IS_OUT_OF_BOUNDS)),
         }
     }
 }
@@ -25,6 +38,8 @@ impl Parse for BytePattern {
 
         if lookahead.peek(LitChar) {
             BytePattern::parse_from_char_literal(input)
+        } else if lookahead.peek(LitInt) {
+            BytePattern::parse_from_int_literal(input)
         } else {
             Err(lookahead.error())
         }
@@ -35,7 +50,7 @@ impl Parse for BytePattern {
 mod parse_tests {
     use super::*;
 
-    curry_parse_macros!(BytePattern);
+    curry_parse_macros!($BytePattern);
 
     #[test]
     fn parse_char_literal() {
@@ -45,8 +60,29 @@ mod parse_tests {
     }
 
     #[test]
-    fn parse_char_literal_range_error() {
+    fn parse_non_ascii_char_literal_error() {
         assert_eq!(parse_err! { '£' }, ERR_CHAR_IS_NOT_ASCII);
         assert_eq!(parse_err! { '🐼' }, ERR_CHAR_IS_NOT_ASCII);
+    }
+
+    #[test]
+    fn parse_int_literal() {
+        assert_eq!(parse_ok! { 0x61 }, BytePattern(0x61));
+        assert_eq!(parse_ok! { 62 }, BytePattern(0x3e));
+        assert_eq!(parse_ok! { 48u64 }, BytePattern(0x30));
+    }
+
+    #[test]
+    fn parse_int_literal_outside_byte_range_error() {
+        assert_eq!(parse_err! { 0x777 }, ERR_INT_IS_OUT_OF_BOUNDS);
+        assert_eq!(parse_err! { 256 }, ERR_INT_IS_OUT_OF_BOUNDS);
+    }
+
+    #[test]
+    fn parse_unexpected_token_error() {
+        assert_eq!(
+            parse_err! { -3 },
+            "expected character literal or integer literal"
+        );
     }
 }
